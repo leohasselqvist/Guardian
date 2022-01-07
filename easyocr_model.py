2a3a50bd69c83b2cef8dbe547255dc0f2a2cc933
Leo <leo.hasselqvist@elev.ntig.se>
2022-01-07 12:55:58 +0100

    Added untested client and server classes

--- easyocr_model.py
+++ easyocr_model.py
@@ -5,6 +5,26 @@ from PIL import ImageDraw
 
 letterset = string.ascii_uppercase + string.digits
 
+reader = easyocr.Reader(['en'])
+
+vid_capture = cv2.VideoCapture(0)
+
+
+def verify_reg(reg):
+    if len(reg) == 6:
+        for i in range(0, len(reg)):
+            if i < 3:
+                if reg[i] not in string.ascii_uppercase:
+                    return False
+            elif i < 5:
+                if reg[i] not in string.digits:
+                    return False
+            else:
+                if reg[i] not in letterset:
+                    return False
+        return True
+    return False
+
 
 def cleanup_text(text):
     # strip out non-ASCII text so we can draw the text on the image
@@ -13,34 +33,51 @@ def cleanup_text(text):
 
 
 def __main__():
-    reader = easyocr.Reader(['en'])
-
-    vid_capture = cv2.VideoCapture(0)
     while True:
-
-        _, image = vid_capture.read()
-
-        bound = reader.readtext(image)
-
-        for (bbox, text, prob) in bound:
-            # display the OCR'd text and associated probability
-            print("[INFO] {:.4f}: {}".format(prob, text))
-            # unpack the bounding box
-            (tl, tr, br, bl) = bbox
-            tl = (int(tl[0]), int(tl[1]))
-            tr = (int(tr[0]), int(tr[1]))
-            br = (int(br[0]), int(br[1]))
-            bl = (int(bl[0]), int(bl[1]))
-            # cleanup the text and draw the box surrounding the text along
-            # with the OCR'd text itself
-            text = cleanup_text(text)
-            cv2.rectangle(image, tl, br, (0, 255, 0), 2)
-            cv2.putText(image, text, (tl[0], tl[1] - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
-        # show the output image
-        cv2.imshow("Image", image)
+        print(read_text())
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
+
+
+def bounding_box_visualizer():  # Use cv2.waitkey() after function
+    _, image = vid_capture.read()
+    all_objects = []
+
+    bound = reader.readtext(image)
+
+    for (bbox, text, prob) in bound:
+        # display the OCR'd text and associated probability
+        # print("[INFO] {:.4f}: {}".format(prob, text))
+        # unpack the bounding box
+        (tl, tr, br, bl) = bbox
+        tl = (int(tl[0]), int(tl[1]))
+        tr = (int(tr[0]), int(tr[1]))
+        br = (int(br[0]), int(br[1]))
+        bl = (int(bl[0]), int(bl[1]))
+        # cleanup the text and draw the box surrounding the text along
+        # with the OCR'd text itself
+        text = cleanup_text(text)
+        all_objects.append(text)
+        color = (0, 0, 255)  # RED
+        if verify_reg(text):
+            color = (0, 255, 0)  # GREEN
+        cv2.rectangle(image, tl, br, color, 2)
+        cv2.putText(image, text, (tl[0], tl[1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
+    # show the output image
+    cv2.imshow("Image", image)
+    return all_objects
+    # if cv2.waitKey(1) & 0xFF == ord("q"):
+    #     break
+
+
+def read_text():
+    _, image = vid_capture.read()
+    data = reader.readtext(image)  # Get raw information
+    output = []
+    for (_, text, _) in data:
+        output.append(text)
+    return output
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-import easyocr_model
+import easyocr_model as ocr
 import threading
-import tesseract
+import cv2
+import string
+import asyncore
 
 #  DENNA KODEN KÖR PÅ RASPBERRY PI:n
 
 
-easyocr_m = easyocr_model.__main__
-tesseract_m = tesseract.__main__
+class SauronReciever(asyncore.dispatcher):
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket()
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(5)
+
+    def handle_accepted(self):
+        print("Signal Inkommande")
+        print(self.recv(1024))
+
+
+def __main__():
+    client = SauronReciever("localhost", 3623)
+    asyncore.loop()
+    paid_cars = []
+    parked_cars = {}  # KEY: str | VALUE: bool (verified or nah)
+    while True:
+        text = ocr.bounding_box_visualizer()
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+        for entry in text:
+            if ocr.verify_reg(entry):  # If the entry is a car
+                parked_cars[entry] = entry in paid_cars  # Add the entry to the list, auto check if they have paid yet.
+
 
 if __name__ == "__main__":
-    easyocr_m()
+    __main__()
