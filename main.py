bc43a0e739b6ab6a553d39e7dc8cddeb456f3172
Leo Hasselqvist <leo.hasselqvist@elev.ntig.se>
2022-01-11 07:15:51 +0100

    Completely flipped the client server model

--- main.py
+++ main.py
@@ -1,52 +1,40 @@
-import easyocr_model as ocr
 import threading
 import cv2
 import string
 import socket
 import sys
 import threading
+import easyocr_model as ocr
 
 #  DENNA KODEN KÖR PÅ RASPBERRY PI:n
 
-paid_cars = []
-parked_cars = {}  # KEY: str | VALUE: bool (verified or nah)
+
+# DEBUG VARIABLES (they look nice)
+
+
+parked_cars = []
+
+HOST = 'localhost'  # The server's hostname or IP address
+PORT = 11417        # The port used by the server
 
 
 def __main__():
-    ocr_trd = threading.Thread(target=run_ocr)  # Create a separate thread for the OCR
-    if "-n" not in sys.argv:  # If -n in args, ignore networking setup
-        print("[OCR] Starting thread...")
-        ocr_trd.start()  # OCR will run in the background
-        print("[NET] Starting server...")
+    if "-n" not in sys.argv:  # If -n in not args, proceed with network
+        print("[NET] NET setup initiated.")
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(('localhost', 11417))  # Set port
-            s.listen()
-            while True:
-                print("[NET] Listening for client...")
-                conn, addr = s.accept()
-                with conn:  # If we get a connection...
-                    print(f'[NET] Client accepted {addr[0]}')
-                    while True:
-                        try:
-                            data = conn.recv(1024).decode('utf-8')  # Data received
-                            print(f"[NET] Received {data}")
-                            if not data:  # If they forcefully closed
-                                print(f"[NET] Connection forcefully closed by {addr[0]}.")
-                                break
-                            if data == "quit":  # If they gracefully closed
-                                conn.close()
-                                print(f"[NET] Connection closed by {addr[0]}")
-                                break
-                            cars = data.split('|')  # Split for multiple cars
-                            for entry in cars:
-                                car = entry.split('.')  # car[0] is the regplate, car[1] is to add or remove it
-                                if car[1] == "1":
-                                    paid_cars.append(car[0].upper())
-                                else:
-                                    paid_cars.remove(car[0].upper())
-                            conn.sendall(str(paid_cars).encode('utf-8'))  # Respond with the current list of paid cars.
-                        except Exception as e:
-                            conn.sendall(("ERROR: " + str(e)).encode('utf-8'))
+            for attempts in range(1, 101):
+                try:
+                    s.connect((HOST, PORT))
+                    print("[NET] Connection established.")
+                    s.send("c".encode('utf-8'))  # Server verification for camera.
+                    running = True
+                    while running:
+                        running = ocr_cycle(s=s)
+                        attempts = 0
+                except ConnectionRefusedError:
+                    print(f"[NET] Connection attempt #{attempts}")
+            else:
+                print("[NET] Timed out")
     else:
         print("[NET] Ignoring NET setup")
         run_ocr()
@@ -59,25 +47,28 @@ def run_ocr():
         ocr_result = ocr_cycle()
 
 
-def ocr_cycle():
+def ocr_cycle(s=None):
     text = ocr.bounding_box_visualizer(debug_text=str(parked_cars))
-    chopping_block = []  # Quickfire solution to dict iteration size issue. Not very elegant
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    if cv2.waitKey(1) & 0xFF == ord("q"):  # If q is pressed...
+        if s:
+            s.close()  # Close connection.
         return False  # Failed cycle, interrupted by keyboard stroke
+    for car in parked_cars:
+        if car not in text:
+            print(f"[OCR] {car} left. ")
+            parked_cars.remove(car)
+            if s:  # If networking is enabled
+                print("[NET] Removing from server...")
+                s.send((car + ".0").encode('utf-8'))
     for entry in text:
         if ocr.verify_reg(entry):  # If the entry is a car
-            parked_cars[entry] = entry in paid_cars  # Add the entry to the list, auto check if they have paid yet.
-            print(f"[DB] Adding {entry} to DB, payment {entry in paid_cars}")
-    for entry in parked_cars:  # Check the existing list of cars and see if any have left
-        if entry not in text and ocr.verify_reg(entry):  # If a car has left...
-            chopping_block.append(entry)  # Add to chopping_block
-
-            # The reason for adding the dead cars to a new list is that a dict cannot change size while being iterated.
-            # chopping_block is a quick and inefficient solution. Will fix later.
-
-    for dead_car in chopping_block:  # TODO: Remove this unnecessary loop.
-        parked_cars.pop(dead_car)  # Remove all cars in chopping_block from parked_cars
-        print(f"[DB] Removing {dead_car} from DB")
+            print(f"[OCR] {entry} identified.")
+            if entry not in parked_cars:
+                print(f"[OCR] {entry} found.")
+                parked_cars.append(entry)
+                if s:
+                    print("[NET] Adding to server...")
+                    s.send((entry + ".1").encode('utf-8'))
     return True  # Completed cycle
 
 
---
+++ server.py
@@ -0,0 +1,61 @@
+import socket
+import datetime
+import time
+
+HOST = 'localhost'  # The server's hostname or IP address
+PORT = 11417        # The port used by the server
+
+parked_cars = []
+paid_cars = {}
+
+
+def camera(c, a):
+    print(f"[CAM] Camera {a[0]} verified")
+    while True:
+        data = c.recv(1024).decode('utf-8')
+        if not data:
+            c.close()
+            print(f"[CAM] Connection closed by {a[0]}")
+            break
+        car = data.split('.')  # car[0] is the regplate, car[1] is to add or remove it
+        if car[1] == "1":
+            parked_cars.append(car[0].upper())
+            print(f"[CAM] {car[0]} parked.")
+        else:
+            try:
+                parked_cars.remove(car[0].upper())
+                print(f"[CAM] {car[0]} left.")
+            except ValueError:
+                print(f"[CAM] OVERLOAD I GUESS, VALUE WAS {car[0]}")
+        print(f"[CAM] {parked_cars}")
+
+
+def networking_head():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        while True:
+            print("[HEAD] Listening for client...")
+            s.listen(5)  # Now wait for client connection.
+            while True:
+                conn, addr = s.accept()
+                print(f"[HEAD] Client {addr[0]} accepted")
+                time.sleep(1)  # Buffer time
+                while True:
+                    data = conn.recv(1024).decode('utf-8')
+                    if data == "c":
+                        camera(conn, addr)
+                        break
+                    else:
+                        park_info = data.split('|')
+                        reg = park_info[0]
+                        expiry_time = datetime.datetime.now() + datetime.timedelta(minutes=int(park_info[1]))
+                        paid_cars[reg] = expiry_time
+
+
+def __main__():
+    networking_head()
+
+
+if __name__ == "__main__":
+    __main__()
